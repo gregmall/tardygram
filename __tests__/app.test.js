55c252d27c6bbfdd3fb119ee018e0066f5094be8
Greg Mall <gregmall157@gmail.com>
2020-09-28 16:23:43 -0700

    second route written and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const userService = require('../lib/services/user-service');
 
 describe('tardygram routes', () => {
   beforeEach(() => {
@@ -17,16 +18,35 @@ describe('tardygram routes', () => {
         password: 'hashbuds',
         profilePhotoUrl: 'www.picture.com'
       });
-console.log(response.body);
+
     expect(response.body).toEqual({
       id: expect.any(String),
       email: 'email@email.com',
       profilePhotoUrl: 'www.picture.com'
     });
 
+  });
 
-  
-
+  it('logs in a user via POST', async() => {
+    const user = await userService.create({
+      email: 'email@email.com',
+      password: 'hashbuds',
+      profilePhotoUrl: 'www.picture.com'
+    });
+    
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'email@email.com',
+        password: 'hashbuds',
+        profilePhotoUrl: 'www.picture.com'
+      });
+      console.log(response.body)
+    expect(response.body).toEqual({
+      id: user.id,
+      email: 'email@email.com',
+      profilePhotoUrl: 'www.picture.com'
+    });
   });
 
 });
